e63d88969020cd661505d059607003fbfa84a156
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-07-25 15:41:21 +0000

    added dog detail page

--- dogs/urls.py
+++ dogs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_dogs, name='dogs')
+    path('', views.all_dogs, name='dogs'),
+    path('<dog_id>', views.dog_detail, name='dog_detail'),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Dog
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_dogs(request):
     }
 
     return render(request, 'dogs/dogs.html', context)
+
+
+def dog_detail(request, dog_id):
+    """ A view to show individual dog details """
+
+    dog = get_object_or_404(Dog, pk=dog_id)
+
+    context = {
+        'dog': dog,
+    }
+
+    return render(request, 'dogs/dog_detail.html', context)
