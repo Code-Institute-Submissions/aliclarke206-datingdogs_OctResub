e58cf9b325862927ea824f4813e94f8eab0bef98
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-07-30 22:41:28 +0000

    added forms and admin

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Subscription
 # Register your models here.
+
+
+class subscriptionAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('member_number', 'date',
+                       'grand_total',)
+
+    fields = ('member_number', 'date', 'full_name',
+              'email', 'town_or_city', 'county', 'grand_total',)
+
+    list_display = ('member_number', 'date', 'full_name',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Subscription)
