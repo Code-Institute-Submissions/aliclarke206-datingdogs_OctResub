ef9c47f247b7f342c95a0ad66da257c2e8f1f3a5
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-07-31 17:12:47 +0000

    added stripe js

--- subscribe/views.py
+++ subscribe/views.py
@@ -5,12 +5,14 @@ from .forms import SubscriptionForm
 # Create your views here.
 
 
-def view_subscribe(request):
+def subscribe(request):
     """ A view that renders the subscribe page """
     subscription_form = SubscriptionForm()
     template = 'subscribe/subscribe.html'
     context = {
         'subscription_form': subscription_form,
+        'stripe_public_key': 'pk_test_51JJKYXLhOLwgGIo6FooM9njTNm2yq3fSk27ubLKTXnkC1Pryh7w7Zr0FXYE86HsDaF2v37HZ8MMcbzLW46jcyz9K00cjiI9Ci4',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
