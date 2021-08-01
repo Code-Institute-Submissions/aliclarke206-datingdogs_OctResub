a8c0366419fd8889575ce1dcb3d483588774671e
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-08-01 16:37:53 +0000

    added event handlers

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.subscribe, name='subscribe'),
     path('subscribe_success/<member_number>', views.subscribe_success, name='subscribe_success'),
+    path('wh/', webhook, name='webhook'),
 ]
