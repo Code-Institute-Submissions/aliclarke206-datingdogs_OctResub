26fc36a45b3e1cf1759a8450b9f5049018fe34f7
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-08-01 09:21:22 +0000

    added saving info to subscribe views

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import SubscriptionForm
+from .models import Subscription
+from dogs.models import Dog
 import stripe
 
 # Create your views here.
@@ -12,15 +14,36 @@ def subscribe(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    if request.method == 'POST':
 
-    fee = round(35 * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=fee,
-        currency=settings.STRIPE_CURRENCY,
-    )
-    """ A view that renders the subscribe page """
-    subscription_form = SubscriptionForm()
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'country': request.POST['country'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+        }
+
+        subscription_form = SubscriptionForm(form_data)
+        if subscription_form.is_valid():
+            subscription = subscription_form.save()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('subscribe_success', args=[subscription.member_number]))
+
+         else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+
+        fee = round(35 * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=fee,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        """ A view that renders the subscribe page """
+        subscription_form = SubscriptionForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -32,6 +55,24 @@ def subscribe(request):
         'subscription_form': subscription_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+def subscribe_success(request, member_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    subscribe = get_object_or_404(Subscription, member_number=member_number)
+    messages.success(request, f'Subscription successfully processed! \
+        Your order number is {member_number}. A confirmation \
+        email will be sent to {subscribe.email}.')
+
+
+    template = 'subscribe/subscribe_success.html'
+    context = {
+        'subscribe': subscribe,
     }
 
     return render(request, template, context)
