95ecd20891b4094784377979923a719eae4c6c73
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-08-02 12:43:33 +0000

    added js for customer info through stripe payment intent

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -4,6 +4,9 @@ from .webhooks import webhook
 
 urlpatterns = [
     path('', views.subscribe, name='subscribe'),
-    path('subscribe_success/<member_number>', views.subscribe_success, name='subscribe_success'),
+    path('subscribe_success/<member_number>', views.subscribe_success,
+         name='subscribe_success'),
+    path('cache_subscribe_data/', views.cache_subscribe_data, 
+         name='cache_subscribe_data'),
     path('wh/', webhook, name='webhook'),
 ]
