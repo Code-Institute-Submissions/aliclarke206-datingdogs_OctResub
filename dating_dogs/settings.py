336f3a6ce850e15a5901577dc0c524acc639d1c8
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-08-06 13:43:19 +0000

    removed secret key

--- dating_dogs/settings.py
+++ dating_dogs/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-po1m-n!toihnvewo+4cj(ll_2gqq5uuh%+%8^1ehbn@16vbz=1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
