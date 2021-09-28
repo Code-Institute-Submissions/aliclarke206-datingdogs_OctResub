1cf3f88dccba72b27e696877ed1633b110194f63
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-09-28 14:54:49 +0000

    added phone contact to models so users can leave their contact info

--- dogs/models.py
+++ dogs/models.py
@@ -28,6 +28,7 @@ class Dog(models.Model):
     description = models.TextField()
     location = models.CharField(max_length=254, null=True, blank=True)
     age = models.DecimalField(max_digits=6, decimal_places=0)
+    phone = models.CharField(max_length=254, null=True, blank=True)
     type = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
