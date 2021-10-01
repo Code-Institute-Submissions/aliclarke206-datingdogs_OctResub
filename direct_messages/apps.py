5477b34bf158628a5dab66b42fbf0f9bb1cf9218
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-10-01 17:34:14 +0000

    deleted redundant code, direct_messages app that wasnt implemented

--- direct_messages/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Direct_MessageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'direct_messages'
--- direct_messages/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.direct_message, name='direct_message')
-]
