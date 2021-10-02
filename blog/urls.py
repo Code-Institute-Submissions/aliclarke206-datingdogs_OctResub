1070fe164036a22fe9a1e63fe1e795595648aef2
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-10-02 21:08:22 +0000

    added form.py and corresponding widgets

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogView, ArticleDetailView
+from .views import BlogView, ArticleDetailView, AddPostView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 class BlogView(ListView):
@@ -11,3 +12,9 @@ class BlogView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'blog/article_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/add_post.html'
