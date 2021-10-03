eaae294645b548cf546968db54294db1cf5db0a8
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-10-03 09:01:19 +0000

    added edit and delete functions to blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogView, ArticleDetailView, AddPostView
+from .views import (BlogView, ArticleDetailView, AddPostView, UpdatePostView,
+                    DeletePostView)
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/<int:pk>', ArticleDetailView.as_view(),
+         name='article_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(),
+         name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(),
+         name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView,
+                                  DeleteView)
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, UpdateForm
+from django.urls import reverse_lazy
 
 
 class BlogView(ListView):
     model = Post
     template_name = 'blog/blog.html'
+    ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -18,3 +21,16 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/add_post.html'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = UpdateForm
+    template_name = 'blog/update_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    form_class = UpdateForm
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('blog')
