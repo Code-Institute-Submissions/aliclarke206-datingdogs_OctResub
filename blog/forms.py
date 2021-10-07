e3dcc2c05c9ad8e46be38f10b374134510171662
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-10-07 23:08:00 +0000

    edited butons on blog and added back buttin to edit post. Also changed form.py back to include drop down of users

--- blog/forms.py
+++ blog/forms.py
@@ -5,10 +5,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'body')
+        fields = ('title', 'author','body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
